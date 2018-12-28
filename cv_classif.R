0f306543528bea0a4e2f35084bd15da6cd9b144f
antoinecollas <antoine.collas@etu.utc.fr>
2018-12-28 15:04:32 +0100

    forêts aléatoires astronomy + rapport

--- cv_classif.R
+++ cv_classif.R
@@ -2,6 +2,7 @@ require(MASS)
 library(e1071)
 library(class)
 library(kernlab)
+library(randomForest)
 
 #fonction réalisant la validation croisée (avec K=10 par défaut) 
 CV_eval <- function(model, data, hyperparameters=c(), fold=10){
@@ -51,6 +52,20 @@ CV_eval <- function(model, data, hyperparameters=c(), fold=10){
       model.pred <- predict(model.fit, test_x)
       errors[k] <- sum(test_y!=model.pred) / length(model.pred)
       mean <- mean + (errors[k]*length(model.pred))
+    }else if (model=='rf'){
+      train <- data[folds!=k, ]
+      test_x <- data[folds==k, -which(names(data)=="y")]
+      test_y <- as.factor(data[folds==k,]$y)
+      if ("mtry" %in% attributes(hyperparameters)$names){
+        mtry <- hyperparameters$mtry
+      }else{
+        mtry <- floor(sqrt(ncol(test_x)))
+      }
+      ntree <- hyperparameters$ntree
+      rf <- randomForest(y~., data=train, mtry=mtry, ntree=ntree)
+      model.pred <- predict(rf, newdata = test_x, type = "response")
+      errors[k] <- sum(test_y!=model.pred) / length(model.pred)
+      mean <- mean + (errors[k]*length(model.pred))
     }else{
       stop('Le modèle demandé n\'est pas implémenté!')
     }
@@ -59,4 +74,3 @@ CV_eval <- function(model, data, hyperparameters=c(), fold=10){
   sd <- sqrt((1/(K-1))*sum((errors - rep(mean, K))^2))
   return(c(mean, sd))
 }
-
